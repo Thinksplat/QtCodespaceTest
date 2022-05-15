2773acc6818773743fc681cbca120171c40c2c26
John Aughey <jha@aughey.com>
2022-05-15 03:03:30 +0000

    refacdtoring tests

--- tests/clickbusiness_tests.cpp
+++ tests/clickbusiness_tests.cpp
@@ -3,65 +3,81 @@
 #include "clickbusiness.h"
 
 // Demonstrate some basic assertions.
-TEST(ClickBusiness, BasicInstaination) {
+TEST(ClickBusiness, BasicInstaination)
+{
     ClickBusiness clicker(10);
     IClickMsg &c = clicker; // Make sure it is of the right type
     (void)c;
 }
 
-TEST(ClickBusiness, MessageStartsEmpty) {
+TEST(ClickBusiness, MessageStartsEmpty)
+{
     ClickBusiness clicker(10);
-    ASSERT_EQ(clicker.Str(),"");
+    ASSERT_EQ(clicker.Str(), "");
 }
 
-TEST(ClickBusiness, OneClickChangesMessage) {
+TEST(ClickBusiness, OneClickChangesMessage)
+{
     ClickBusiness clicker(10);
     auto oldstr = clicker.Str();
     clicker.Click();
     auto newstr = clicker.Str();
-    ASSERT_NE(oldstr,newstr);
+    ASSERT_NE(oldstr, newstr);
 }
 
-TEST(ClickBusiness, TwoClicksHaveTwoLinesInTheMessage) {
+static unsigned int countLines(const std::string &msg)
+{
+    return std::count(msg.begin(), msg.end(), '\n');
+}
+
+static unsigned int countLines(IClickMsg &clicker)
+{
+    auto str = clicker.Str();
+    return countLines(str);
+}
+
+TEST(ClickBusiness, TwoClicksHaveTwoLinesInTheMessage)
+{
     ClickBusiness clicker(10);
     clicker.Click();
     clicker.Click();
-    auto msg = clicker.Str();
-    auto newlinecount = std::count(msg.begin(), msg.end(), '\n');
-    ASSERT_EQ(newlinecount,2);
+    auto newlinecount = countLines(clicker);
+    ASSERT_EQ(newlinecount, 2);
 }
 
-TEST(ClickBusiness, MessageOverflowProducesOneLineAfterLast) {
+TEST(ClickBusiness, MessageOverflowProducesOneLineAfterLast)
+{
     ClickBusiness clicker(5);
     clicker.Click();
     clicker.Click();
     clicker.Click();
     clicker.Click();
     clicker.Click();
-    auto msg = clicker.Str();
-    auto newlinecount = std::count(msg.begin(), msg.end(), '\n');
-    ASSERT_EQ(newlinecount,5);
+    auto newlinecount = countLines(clicker);
+    ASSERT_EQ(newlinecount, 5);
 
     clicker.Click();
-    msg = clicker.Str();
-    newlinecount = std::count(msg.begin(), msg.end(), '\n');
-    ASSERT_EQ(newlinecount,1);
+
+    newlinecount = countLines(clicker);
+    ASSERT_EQ(newlinecount, 1);
 }
 
-static std::vector<std::string> splitString(const std::string& str)
+static std::vector<std::string> splitString(const std::string &str)
 {
     std::vector<std::string> tokens;
- 
+
     std::stringstream ss(str);
     std::string token;
-    while (std::getline(ss, token, '\n')) {
+    while (std::getline(ss, token, '\n'))
+    {
         tokens.push_back(token);
     }
- 
+
     return tokens;
 }
 
-TEST(ClickBusiness, LastMessageBeforeOverflowYellsAtMe) {
+TEST(ClickBusiness, LastMessageBeforeOverflowYellsAtMe)
+{
     const std::string angrymessage = "STOP PUSHING THE BUTTON";
     ClickBusiness clicker(5, angrymessage);
     clicker.Click();
@@ -70,12 +86,12 @@ TEST(ClickBusiness, LastMessageBeforeOverflowYellsAtMe) {
     clicker.Click();
     clicker.Click();
     auto msg = clicker.Str();
-    auto newlinecount = std::count(msg.begin(), msg.end(), '\n');
-    ASSERT_EQ(newlinecount,5);
+    auto newlinecount = countLines(msg);
+    ASSERT_EQ(newlinecount, 5);
 
     // Split the string on newline
     auto lines = splitString(msg);
-    ASSERT_EQ(lines.size(),5);
+    ASSERT_EQ(lines.size(), 5);
 
     // The last line needs to have an angry message
     auto lastline = lines.back();
